00dd28a7015f2398b1aeb66b31e16ccd0066646e
Nicolás Renato Ricciardi Acuña <114707269+ricachii@users.noreply.github.com>
2025-11-25 15:55:04 -0300

    cambio de tipo de horaEstimada

--- model/PuntoRuta.java
+++ model/PuntoRuta.java
@@ -1,12 +1,14 @@
 package model;
+import java.time.LocalTime;
 
 public class PuntoRuta {
     private int orden;
     private String horaEstimada;
+    private LocalTime horaEstimada;
     private String estadoVisita;
     private Solicitud solicitud;
 
-    public PuntoRuta(int orden, String horaEstimada, String estadoVisita) {
+    public PuntoRuta(int orden, LocalTime horaEstimada, String estadoVisita) {
         this.orden = orden;
         this.horaEstimada = horaEstimada;
         this.estadoVisita = estadoVisita;
@@ -15,8 +17,8 @@ public class PuntoRuta {
     // Getters y setters
     public int getOrden() { return orden; }
     public void setOrden(int orden) { this.orden = orden; }
-    public String getHoraEstimada() { return horaEstimada; }
-    public void setHoraEstimada(String horaEstimada) { this.horaEstimada = horaEstimada; }
+    public LocalTime getHoraEstimada() { return horaEstimada; }
+    public void setHoraEstimada(LocalTime HoraEstimada) { this.horaEstimada = horaEstimada; }
     public String getEstadoVisita() { return estadoVisita; }
     public void setEstadoVisita(String estadoVisita) { this.estadoVisita = estadoVisita; }
     public Solicitud getSolicitud() { return solicitud; }
